09bc4aed7e16c7a20fc73c8926b609db4606f144
Braeden <stormwizard1001@gmail.com>
2022-08-08 01:46:36 -0500

    Delete cmake-build-debug directory
    Former-commit-id: 499251f563ec4574201e863a9b991782927669e4
    Former-commit-id: cc52efbeb105f1b336368598eb79f91f975fd778
    Former-commit-id: ccf609c351195795e0e490d23ea30fb33295fc84
    Former-commit-id: c6a41d8d298d789ed5bc853bf2b760633c2e23b0

--- cmake-build-debug/luau/CMakeFiles/Luau.Ast.CLI.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Luau.Ast.CLI.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/luau/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/braedenweaver/CLionProjects/luau-decompiler/luau
-# Build directory: /Users/braedenweaver/CLionProjects/luau-decompiler/cmake-build-debug/luau
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
