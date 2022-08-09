8721f3def0a4e21a1b0fb26158bb08e12d6ae0a5
Braeden <stormwizard1001@gmail.com>
2022-08-09 17:16:36 -0600

    fix deps
    Former-commit-id: a23f0f4056ba6cbf932f70826e6e5654cea8c66f
    Former-commit-id: 452f0e80c0a2a566a43625ca19376d277a111efa
    Former-commit-id: 3f8d6b7bf88b8f2e9ee9ea4b7e13e0b72da5626e
    Former-commit-id: db0401ccb38b859124357bdb0ed51bf8a4f2e019

--- Decompiler/Decompile.h
+++ Decompiler/Decompile.h
@@ -1,12 +1,15 @@
 //
 // Created by xgladius on 8/7/22.
 //
+#pragma once
 #include <iostream>
 #include <Luau/Transpiler.h>
 #include "BlockGen/include/BlockGen.h"
 #include "Common.h"
 
 namespace Luau::Decompiler {
+    void dumpInstructionX(const uint32_t* code, std::string& result, int targetLabel);
+
     void dumpProto(lua_State* L, Proto* p);
 
     std::string decompile(lua_State* L, std::string& bytecode);
