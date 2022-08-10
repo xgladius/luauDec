c5c6e5a4ac7d0b0e485f1ce03d905355421bfa72
Braeden <stormwizard1001@gmail.com>
2022-08-09 23:26:39 -0600

    Delete out/build/x64-Debug directory
    Former-commit-id: c107805476455032c0bfe4030d9634fcfc6208eb
    Former-commit-id: 5f7edb3f1a7f4a983868628d7fd823d02677ad85
    Former-commit-id: e404b0c07d2a3cb0740c7f4b001456b5619d56c8
    Former-commit-id: 13654b818603e95923af957b40c9d29a1d7bcc03

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/xgladius/Documents/luauDec
-# Build directory: C:/Users/xgladius/Documents/luauDec/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("luau")
